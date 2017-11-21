6fa82f4fe40f08f4d3a4b9db71f1522779e44ede
PohSayKeong <poh.saykeong@dhs.sg>
2017-11-21 15:01:27 +0800

    Label Name Change for 2nd angle

---
+++ labelNameChange (2).py
@@ -0,0 +1,12 @@
+allLabels = []
+with open('Label.txt') as f:
+    for line in f:
+        label = line.split()
+        allLabels.append(label)
+print(allLabels[631])
+print(len(allLabels))
+for i in range(len(allLabels)):
+    file = open(allLabels[i][0] + " (2).jpg.txt","w")
+    file.write('\n'.join(allLabels[i][1:]))
+    file.close() 
+ 
