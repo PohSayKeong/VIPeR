7ba60be8628d8cd909af126a7dcde247ebc3faf8
PohSayKeong <poh.saykeong@dhs.sg>
2017-11-21 14:59:38 +0800

    Use current directory

--- imgChange.py
+++ imgChange.py
@@ -1,5 +1,5 @@
 import os,sys
-folder = 'C:/Users/pohsa/Desktop/test folder/images'
+folder = os.cwd()
 for filename in os.listdir(folder):
        infilename = os.path.join(folder,filename)
        if not os.path.isfile(infilename): continue
